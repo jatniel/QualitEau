5a79c6b093ccb52837d2147a51cbc3a20f5eb878
Jatniel Guzmán <1070744+jatniel@users.noreply.github.com>
2025-12-20 22:10:48 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
+/**
+ * Web routes QualitEau
+ * @author    Jatniel Guzmán https://jatniel.dev
+ * @copyright    2025
+ * @license    MIT
+ */
 
+declare(strict_types=1);
+
+use App\Services\HubEau\HubEauClient;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Home page
+ */
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');
+
+/**
+ * Commune water quality details
+ */
+Route::get('/commune/{code}', function (string $code, HubEauClient $client) {
+    $commune = $client->getCommune($code);
+
+    if ($commune === null) {
+        abort(404, 'Commune non trouvée');
+    }
+
+    return view('pages.commune', [
+        'code' => $code,
+        'commune' => (object) $commune,
+    ]);
+})->name('commune.show');
